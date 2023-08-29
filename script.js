f4eaf486c9fa5818b0ab05e8f9560191879c852d
object-ions <moshikoatia@gmail.com>
2023-08-29 10:42:20 -0600

    add an interval of time counting every sec

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const intervalID = setInterval(myCallback, 1000);
+const intervalID = setInterval(countTime, 1000);
 
-function myCallback() {
-  console.log(Date.now());
+function countTime() {
+    const now = new Date(Date.now());
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');  // Months are 0-11, so +1 and pad with a zero if necessary
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    const humanReadableTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    
+    console.log(humanReadableTimestamp);  // Outputs: YYYY-MM-DD HH:MM:SS
+    
 }
