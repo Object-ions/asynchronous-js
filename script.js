795a929e5e2639f8aa561d8ba5ae541a80137f02
object-ions <moshikoatia@gmail.com>
2023-08-29 10:39:03 -0600

    setInerval and clearInterval

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// setTimeout(changeText, 2000);
-// even if we change it to 0 sec it will still be executed second, the reason is bc its added to the 'rtask que'
+const intervalID = setInterval(myCallback, 1000);
 
-
-function changeText() {
-    document.querySelector('h1').textContent = 'Hello from callback';
+function myCallback() {
+  console.log(Date.now());
 }
-
-const timerId = setTimeout(changeText, 3000);
-
-document.querySelector('#cancel').addEventListener('click', () => {
-    console.log(timerId);
-    clearTimeout(timerId);
-    console.log('Timer Cancelled');
-});
