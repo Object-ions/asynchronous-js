59d6ce6bcb3215f28cd90be9fd12db8b0afd6bf9
object-ions <moshikoatia@gmail.com>
2023-08-29 10:29:57 -0600

    setTimeout and clearTimeout in action

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-setTimeout(changeText, 2000); // even if we change it to 0 sec it will still be executed second, the reason is bc its added to the 'rtask que'
+// setTimeout(changeText, 2000);
+// even if we change it to 0 sec it will still be executed second, the reason is bc its added to the 'rtask que'
 
 
 function changeText() {
     document.querySelector('h1').textContent = 'Hello from callback';
 }
+
+const timerId = setTimeout(changeText, 3000);
+
+document.querySelector('#cancel').addEventListener('click', () => {
+    console.log(timerId);
+    clearTimeout(timerId);
+    console.log('Timer Cancelled');
+});
