85fe178f12f761f4591526b6c0d6d86990b532b9
object-ions <moshikoatia@gmail.com>
2023-08-29 10:25:06 -0600

    used setTimeOut w a func.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-setTimeout(() => {
-    console.log('Hello from callback');
-}, 2000); // even if we change it to 0 sec it will still be executed second, the reason is bc its added to the 'rtask que'
+setTimeout(changeText, 2000); // even if we change it to 0 sec it will still be executed second, the reason is bc its added to the 'rtask que'
 
-console.log('Hello from Global Scope');
+
+function changeText() {
+    document.querySelector('h1').textContent = 'Hello from callback';
+}
