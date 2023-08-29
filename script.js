3b85f6183222d704ede5e40c8a45ae96ae27d6f6
object-ions <moshikoatia@gmail.com>
2023-08-29 11:10:14 -0600

    add intevakl and clear interval on cliock. the func. will color change from blacktp white every 1 sec

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-const intervalID = setInterval(countTime, 1000);
+// const intervalID = setInterval(countTime, 1000);
 
-function countTime() {
-    const now = new Date(Date.now());
+// function countTime() {
+//     const now = new Date(Date.now());
 
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');  // Months are 0-11, so +1 and pad with a zero if necessary
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+//     const year = now.getFullYear();
+//     const month = String(now.getMonth() + 1).padStart(2, '0');  // Months are 0-11, so +1 and pad with a zero if necessary
+//     const day = String(now.getDate()).padStart(2, '0');
+//     const hours = String(now.getHours()).padStart(2, '0');
+//     const minutes = String(now.getMinutes()).padStart(2, '0');
+//     const seconds = String(now.getSeconds()).padStart(2, '0');
     
-    const humanReadableTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+//     const humanReadableTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     
-    console.log(humanReadableTimestamp);  // Outputs: YYYY-MM-DD HH:MM:SS
+//     console.log(humanReadableTimestamp);  // Outputs: YYYY-MM-DD HH:MM:SS
     
+// }
+
+let intervalID;
+
+function startChage() {
+    console.log('Starting interval');
+    if (!intervalID) {
+        intervalID = setInterval(changeColor, 1000);
+    }
 }
+
 
 // Stop the interval
 function stopChage() {
+    console.log('Stopping interval');
     clearInterval(intervalID);
 }
 
+function changeColor() {
+    if (document.body.style.backgroundColor !== 'black') {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+}
+
 // Event listeners
+document.getElementById('start').addEventListener('click', startChage);
 document.getElementById('cancel').addEventListener('click', stopChage);
